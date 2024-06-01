27d21723f3d2ddceef36af725b0cedffd1968794
Ekarika Nsemeke <hamatthan@gmail.com>
2024-06-01 17:53:04 +0100

    verified all contracts on https://www.oklink.com/amoy/

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,27 +4,29 @@ import "dotenv/config";
 import { vars } from "hardhat/config";
 
 // npx hardhat vars set OKLINKS_API_KEY 
+// Example Link to one of the contract
+// https://www.oklink.com/amoy/address/0x310d27ddd9dc9693b7a98d77d4ff528ffb221c61/contract#category=read
 const OKLink_API_KEY = vars.get("OKLink_API_KEY");
 
 const config: HardhatUserConfig = {
  solidity: "0.8.20",
  networks: {
-    amoy: {
+    polygonAmoy: {
       url: process.env.URL,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
  },
  etherscan: {
     apiKey: {
-      amoy: OKLink_API_KEY,
+      polygonAmoy: OKLink_API_KEY,
     },
     customChains: [
       {
-        network: "amoy",
+        network: "polygonAmoy",
         chainId: 80002,
         urls: {
-          apiURL: "https://www.oklink.com/amoy/api",
-          browserURL: "https://www.oklink.com/amoy",
+          apiURL: "https://www.oklink.com/api/explorer/v1/contract/verify/async/api/polygonAmoy",
+          browserURL: "https://www.oklink.com/polygonAmoy",
         },
       },
     ],
